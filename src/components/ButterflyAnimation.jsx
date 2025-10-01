b77f4df3a16d371adea303b8b2aae6b3f3ade0ab
chofichou <e.sofiac06@gmail.com>
2025-10-01 13:45:36 -0300

    SOY DIOS
    XD

---
+++ src/components/ButterflyAnimation.jsx
@@ -0,0 +1,17 @@
+// ButterflyAnimation.jsx
+import React from "react";
+import "../App.css";
+
+
+const ButterflyAnimation = () => {
+  return (
+    <div className="butterflies">
+      <img src="https://png.pngtree.com/recommend-works/png-clipart/20250429/ourmid/pngtree-pink-butterfly-black-background-png-image_16149618.png" alt="mariposa" className="butterfly b1" />
+      <img src="https://png.pngtree.com/recommend-works/png-clipart/20250429/ourmid/pngtree-pink-butterfly-black-background-png-image_16149618.png" alt="mariposa" className="butterfly b2" />
+      <img src="https://png.pngtree.com/recommend-works/png-clipart/20250429/ourmid/pngtree-pink-butterfly-black-background-png-image_16149618.png" alt="mariposa" className="butterfly b3" />
+      <img src="https://png.pngtree.com/recommend-works/png-clipart/20250429/ourmid/pngtree-pink-butterfly-black-background-png-image_16149618.png" alt="mariposa" className="butterfly b4" />
+    </div>
+  );
+};
+
+export default ButterflyAnimation;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import ButterflyAnimation from "./ButterflyAnimation";
 
 const productos = [
   "/img/productos/vestido_grecia.png",
@@ -12,17 +13,19 @@ const productos = [
 function home() {
   return (
     <main className="inicio">
+      {/* Animación de mariposas */}
+      <ButterflyAnimation />
+
       <p className="font titulo">Nova Style</p>
       <p className="font subtitulo"> Destacados del mes </p>
 
-        <div className="destacados-container">
-            {productos.map((img, index) => (
-            <div key={index} className="destacados">
-                <img src={img} alt={`Prenda ${index + 1}`} />
-            </div>
-            ))}
-        </div>
-
+      <div className="destacados-container">
+        {productos.map((img, index) => (
+          <div key={index} className="destacados">
+            <img src={img} alt={`Prenda ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
